73d8191b96d400166b4b9b087d355416a8ed29ad
MahithG <mahithgudivaka45@gmail.com>
2026-07-23 19:14:00 +0530

    Time: 0 ms (100.00%) | Memory: 28.4 MB (11.73%) - LeetSync

---
+++ 169-majority-element/majority-element.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int majorityElement(vector<int>& nums) {
+        std::unordered_map<int, int> mpp;
+        for (int i : nums) mpp[i]++ ;
+        for(auto &it : mpp){
+            if(it.second > nums.size()/2){
+                return it.first ;
+            } 
+        }
+  return -1;  }
+};
