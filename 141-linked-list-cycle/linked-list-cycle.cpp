ab68852783633684ab050dd44a4389a2fbd46bc3
MahithG <mahithgudivaka45@gmail.com>
2026-08-12 19:31:41 +0530

    Time: 12 ms (41.96%) | Memory: 14.4 MB (11.13%) - LeetSync

---
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* temp = head ;
+        unordered_set<ListNode*> vis ;
+        while(temp != nullptr){
+            if(vis.count(temp)) return true ;
+            vis.insert(temp) ;
+            temp = temp->next ;
+        }
+   return false ; }
+};
