9c7bb5c9f98b5db02a40b65aeb9ead54c80825ba
MahithG <mahithgudivaka45@gmail.com>
2026-09-04 21:47:54 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (100.00%) - LeetSync

--- 2168-check-if-numbers-are-ascending-in-a-sentence/check-if-numbers-are-ascending-in-a-sentence.cpp
+++ 2168-check-if-numbers-are-ascending-in-a-sentence/check-if-numbers-are-ascending-in-a-sentence.cpp
@@ -1,22 +1,26 @@
 class Solution {
 public:
     bool areNumbersAscending(string s) {
-        vector<int>numst;
-        for(int i = 0;i<s.size();i++){
-            if((i == 0 || s[i-1]==' ')&&isdigit(s[i])){
+        int prev = -1;
+
+        for(int i = 0; i < s.size(); i++) {
+
+            if(isdigit(s[i])) {
+
                 int num = 0;
-                 while(i<s.size() && s[i] != ' ') {
+
+                while(i < s.size() && isdigit(s[i])) {
                     num = num * 10 + (s[i] - '0');
                     i++;
-                } numst.push_back(num);
+                }
 
+                if(num <= prev)
+                    return false;
+
+                prev = num;
             }
         }
-        for(int i = 0; i<numst.size();i++){
-            cout << numst[i] << " ";
-        }
-        for(int i =0;i<numst.size()-1;i++){
-            if(numst[i+1]<=numst[i]) return false ;
-        }
-   return true ; }
+
+        return true;
+    }
 };
